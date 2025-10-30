ecea338c5527529130d29d7011f9bf312b2c6913
Hugo Fenoll <fenollh@gmail.com>
2025-10-30 12:42:02 +0100

    delpoy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,45 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/proyecto_mm/', // 👈 CAMBIA ESTO
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate', // El SW se actualiza automáticamente
+      includeAssets: ['aicon.png'],
+      manifest: {
+        name: 'Motomami',
+        short_name: 'Motomami',
+        description: 'Mi aplicación Vue convertida en PWA',
+        theme_color: '#af150b',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'aicon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'aicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,mp3}'], // cachea todos los archivos importantes
+        navigateFallback: '/', // SPA
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => true, // cachea TODO lo que se pida
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'runtime-cache',
+              expiration: { maxEntries: 200 },
+            },
+          },
+        ],
+      }
+    })
   ],
 })
